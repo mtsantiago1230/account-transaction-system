02f5763dd6435b560405a10ffb300f3fb94b9372
mtsantiago1230 <mtsantiago1032@gmail.com>
2025-10-05 16:07:33 -0500

    fix(backend): refactor code - transaction

--- backend/src/interfaces/controllers/simple-transaction.controller.ts
+++
@@ -1,2 +0,0 @@
-// Deprecated: simple-transaction feature removed. File kept empty to avoid build references.
-export {};
--- backend/src/interfaces/dtos/simple-transaction.dto.ts
+++
@@ -1,2 +0,0 @@
-// Deprecated: simple-transaction DTOs removed. File kept empty to avoid build references.
-export {};
